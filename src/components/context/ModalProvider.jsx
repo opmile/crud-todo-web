7b767364355dff9aa2f328481c87bdc6c0ec524d
opmile <milenaoli2703@gmail.com>
2026-01-06 08:58:51 -0300

    feat: add modal context

---
+++ src/components/context/ModalProvider.jsx
@@ -0,0 +1,28 @@
+import { createContext, useContext, useState } from "react";
+
+const ModalContext = createContext(null)
+
+export function useModal() {
+    const context = useContext(ModalContext)
+
+    if (!context) {
+        throw new Error("useModal must be used within Modal.Root")
+    }
+
+    return context
+}
+
+const ModalProvider = ({ children }) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const openModal = () => setIsOpen(true);
+    const closeModal = () => setIsOpen(false);
+
+    return (
+        <ModalContext value={{ isOpen, openModal, closeModal }}>
+            {children}
+        </ModalContext>
+    );
+}
+
+export default ModalProvider;
